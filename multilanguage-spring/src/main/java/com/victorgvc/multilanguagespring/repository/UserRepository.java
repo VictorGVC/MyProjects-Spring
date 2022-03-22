5095d3abfcfaa2bb7984e2f4b1b30792f7246c5b
victorGVC <victorstudy17@hotmail.com>
2022-03-22 20:37:20 -0300

    feat: Create jpa repositories

---
+++ multilanguage-spring/src/main/java/com/victorgvc/multilanguagespring/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.victorgvc.multilanguagespring.repository;
+
+import com.victorgvc.multilanguagespring.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    
+}
