ea0e48eee21cc137ffb064afe348eb4a7354d277
victorGVC <victorstudy17@hotmail.com>
2022-03-24 17:32:37 -0300

    refactor: minor changes

--- src/main/java/com/victorgvc/multilanguagespring/service/UserService.java
+++ src/main/java/com/victorgvc/multilanguagespring/service/UserService.java
@@ -4,15 +4,13 @@ import java.util.List;
 
 import com.victorgvc.multilanguagespring.model.User;
 
+import org.springframework.http.ResponseEntity;
+
 public interface UserService {
     
-    User save(User user);
-
+    ResponseEntity<?> save(User user);
     List<User> get();
-
     User getById(int id);
-
     User delete(int id);
-
     String encryptPassword(String password);
 }
