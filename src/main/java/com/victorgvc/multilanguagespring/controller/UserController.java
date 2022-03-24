bea7392cae27dcf3753f68b3ffccb83b7ce241f4
victorGVC <victorstudy17@hotmail.com>
2022-03-24 17:47:24 -0300

    feat: Create User Controller

--- src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
@@ -1,70 +1,24 @@
 package com.victorgvc.multilanguagespring.controller;
 
-import java.util.List;
-
 import com.victorgvc.multilanguagespring.model.User;
-import com.victorgvc.multilanguagespring.repository.UserRepository;
 import com.victorgvc.multilanguagespring.service.UserService;
-import com.victorgvc.multilanguagespring.utils.Validations;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
-public class UserController implements UserService{
-
-    private UserRepository repository;
-
-    @Autowired
-    public UserController(UserRepository repository) {
-        super();
-        this.repository = repository;
-    }
-
-    @Override
-    public User save(User user) {
-        try {
-            Validations.notExists(user.getUsername(), "Empty username");
-            Validations.notExists(user.getPassword(), "Empty password");
-            Validations.notExists(user.getConfirmPassword(), "Invalid password confirmation");
-            Validations.notEquals(user.getPassword(), user.getConfirmPassword(), "Password confirmation does not match");
-
-            User userFromDB = repository.findUserByUsername(user.getUsername());
-
-            if(user.getId() == null)
-                Validations.exists(userFromDB, "User already exists");
-        } catch (Exception e) {
-            // TODO return on response an error message
-        }
-
-
-        return null;
-    }
-
-    @Override
-    public List<User> get() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public User getById(int id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public User delete(int id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String encryptPassword(String password) {
-        String salt = BCrypt.gensalt(10);
-        String passEncoded = BCrypt.hashpw(password, salt);
-        return passEncoded;
-    }
+@RestController
+public class UserController {
     
+    private UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> save(@RequestBody User user) {
+        return service.save(user);
+    }
 }
