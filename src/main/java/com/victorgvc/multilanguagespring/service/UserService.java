ee6375cb5a6012752ec1d4c0627c63ff2fa561ee
victorGVC <victorstudy17@hotmail.com>
2022-03-25 09:49:56 -0300

    fix: return types fixed

--- src/main/java/com/victorgvc/multilanguagespring/service/UserService.java
+++ src/main/java/com/victorgvc/multilanguagespring/service/UserService.java
@@ -1,6 +1,7 @@
 package com.victorgvc.multilanguagespring.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.victorgvc.multilanguagespring.model.User;
 
@@ -9,8 +10,8 @@ import org.springframework.http.ResponseEntity;
 public interface UserService {
     
     ResponseEntity<?> save(User user);
-    List<User> get();
-    User getById(int id);
-    User delete(int id);
+    List<User> get() throws Exception;
+    Optional<User> getById(int id) throws Exception;
+    void delete(int id) throws Exception;
     String encryptPassword(String password);
 }
