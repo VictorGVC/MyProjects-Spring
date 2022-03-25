8e03b9dc382f15aebcb77901780d5264f2b222a1
victorGVC <victorstudy17@hotmail.com>
2022-03-25 16:28:44 -0300

    fix: Unsupported Media Type fixed

--- src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,8 +23,8 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> save(@RequestBody User user) {
+    @PostMapping(value = "/signup")
+    public ResponseEntity<?> save(User user) {
         return service.save(user);
     }
 
@@ -52,4 +51,6 @@ public class UserController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
         }
     }
+
+
 }
