de571a3f08f7b1aa784032b3ad3ce70816294c11
victorGVC <victorstudy17@hotmail.com>
2022-03-25 13:17:32 -0300

    feat: Create get users endpoint

--- src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.victorgvc.multilanguagespring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.victorgvc.multilanguagespring.model.User;
 import com.victorgvc.multilanguagespring.service.UserService;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +25,16 @@ public class UserController {
     public ResponseEntity<?> save(@RequestBody User user) {
         return service.save(user);
     }
+
+    @GetMapping("/user")
+    public List<User> get() {
+        try {
+            return service.get();
+        } catch (Exception e) {
+            List<User> error = new ArrayList<>();
+            error.add(new User(500, e.getMessage()));
+            return error;
+        }
+    }
+
 }
