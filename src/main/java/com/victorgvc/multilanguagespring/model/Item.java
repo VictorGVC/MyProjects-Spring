402add1e13740c53fdd1bc52e053a71219f7e5fc
victorGVC <victorstudy17@hotmail.com>
2022-03-28 14:13:45 -0300

    fix: Add identity persistence notation to models

--- src/main/java/com/victorgvc/multilanguagespring/model/Item.java
+++ src/main/java/com/victorgvc/multilanguagespring/model/Item.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -31,6 +33,7 @@ import lombok.ToString;
 public class Item {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "it_id")
     private Integer id;
 
