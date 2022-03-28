1a0b09ddfafa4873f6a129917087877fbcd585ee
victorGVC <victorstudy17@hotmail.com>
2022-03-28 14:19:36 -0300

    feat: Create endpoint to get project by id

--- src/main/java/com/victorgvc/multilanguagespring/controller/ProjectController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/ProjectController.java
@@ -1,11 +1,14 @@
 package com.victorgvc.multilanguagespring.controller;
 
+import java.util.Optional;
+
 import com.victorgvc.multilanguagespring.model.Project;
 import com.victorgvc.multilanguagespring.service.ProjectService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +22,27 @@ public class ProjectController {
     }
 
     @PostMapping(value = "/project")
-    public ResponseEntity<?> save(Project user) {
-        return service.save(user);
+    public ResponseEntity<?> save(Project project) {
+        return service.save(project);
     }
 
     @GetMapping("/project")
     public Object get() {
         try {
             return service.get();
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @GetMapping("/project/{id}")
+    public Object getById(@PathVariable (value = "id") int id) {
+        try {
+            Optional<Project> project = service.getById(id);
+            if(project.isPresent())
+                return project.get();
+            else
+                return new ResponseEntity<>("Project do not found!", HttpStatus.valueOf(500));
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
         }
