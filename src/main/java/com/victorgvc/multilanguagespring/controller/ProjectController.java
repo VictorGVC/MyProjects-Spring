2d2576da6040b823283cebd137ebf08bf7626cbc
victorGVC <victorstudy17@hotmail.com>
2022-03-28 16:37:45 -0300

    feat: Create endpoint for delete project

--- src/main/java/com/victorgvc/multilanguagespring/controller/ProjectController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/ProjectController.java
@@ -7,6 +7,7 @@ import com.victorgvc.multilanguagespring.service.ProjectService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,15 @@ public class ProjectController {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
         }
-        
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> delete(@PathVariable (value = "id") int id) {
+        try {
+            service.delete(id); 
+            return new ResponseEntity<>("Project "+id+" deleted!", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
+        }
     }
 }
