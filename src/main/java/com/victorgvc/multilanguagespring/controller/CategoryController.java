7ca4d9ba64eb1a434df5496fb4708421ddbd34dd
victorGVC <victorstudy17@hotmail.com>
2022-03-29 14:49:35 -0300

    feat: create endpoint to get all categories and get category by id

--- src/main/java/com/victorgvc/multilanguagespring/controller/CategoryController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.victorgvc.multilanguagespring.controller;
 
+import java.util.Optional;
+
 import com.victorgvc.multilanguagespring.model.Category;
 import com.victorgvc.multilanguagespring.service.CategoryService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,28 @@ public class CategoryController {
     @PostMapping(value = "/category")
     public ResponseEntity<?> save(Category category) {
         return service.save(category);
+    }
+
+    @GetMapping("/category")
+    public Object get() {
+        try {
+            return service.get();
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @GetMapping("/category/{id}")
+    public Object getById(@PathVariable (value = "id") int id) {
+        try {
+            
+            Optional<Category> category = service.getById(id);
+            if(category.isPresent())
+                return category.get();
+            else
+                return new ResponseEntity<>("Category do not found!", HttpStatus.valueOf(500));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
+        }
     }
 }
