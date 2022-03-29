7009de5509c7ec9b1be17fcae87caa3357de9524
victorGVC <victorstudy17@hotmail.com>
2022-03-29 15:03:15 -0300

    feat: create enpoint to update and delete category

--- src/main/java/com/victorgvc/multilanguagespring/controller/CategoryController.java
+++ src/main/java/com/victorgvc/multilanguagespring/controller/CategoryController.java
@@ -7,9 +7,11 @@ import com.victorgvc.multilanguagespring.service.CategoryService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,12 +40,28 @@ public class CategoryController {
     @GetMapping("/category/{id}")
     public Object getById(@PathVariable (value = "id") int id) {
         try {
-            
+
             Optional<Category> category = service.getById(id);
             if(category.isPresent())
                 return category.get();
             else
                 return new ResponseEntity<>("Category do not found!", HttpStatus.valueOf(500));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @PutMapping("/category/{id}")
+    public ResponseEntity<?> save(@PathVariable (value = "id") int id, Category category) {
+        category.setId(id); 
+        return service.save(category); 
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<?> delete(@PathVariable (value = "id") int id) {
+        try {
+            service.delete(id); 
+            return new ResponseEntity<>("Category "+id+" deleted!", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(500));
         }
